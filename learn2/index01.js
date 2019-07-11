cefb1e8062c3b5beded8ca392ffcd842e7cdda1c
kheired <644417782@qq.com>
2019-07-12 00:36:25 +0800

    second node project

---
+++ learn2/index01.js
@@ -0,0 +1,8 @@
+var net = require('net');
+var client = net.connect(6667, 'irc.freenode.net');
+client.on('connect', function() {
+  client.wrire('NICK mynick\r\n');
+  client.write('USER mynick 0 * :realname\r\n');
+  client.write('JOIN #node.js\r\n');
+});
+// net.connect('300', 'localhost', function(){});
